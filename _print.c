3e04fdf07bce4ff6dad9627595aa908b95b65f24
segunojo1 <olusegunkd2017@gmail.com>
2023-09-16 20:15:58 +0100

    _printf

---
+++ _print.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _printf - printf function
+ * @format - format specified
+ * Return - formatted output
+*/
+int _printf(const char *format, ...)
+{
+    va_list arguments;
+
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+
+    va_start(arguments, format);
+
+    va_end(arguments);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stdlib.h"
+#include "stdarg.h"
+
+int _printf(const char *format, ...)
+
+#endif
