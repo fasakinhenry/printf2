b5d75040fc94f9bda7454ad2b2b273e382562e5d
segunojo1 <olusegunkd2017@gmail.com>
2023-09-18 15:17:23 -0700

    betty print char

--- print_char.c
+++ print_char.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdarg.h>
 
+/**
+ * print_char - displays a character
+ * @arguments: va_list arguments from _printf
+ * @printed_chars: counts no of chars
+ * Return: number of char printed
+ */
 void print_char(va_list arguments, int *printed_chars)
 {
-    char character = va_arg(arguments, int); 
-    _putchar(character);
-    (*printed_chars)++;
+	char character = va_arg(arguments, int);
+
+	_putchar(character);
+	(*printed_chars)++;
 }
