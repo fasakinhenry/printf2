7f979500e6cb5a4edf85ab01bc40233e9ec1d041
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-18 18:17:20 +0100

    Fixed betty errors

--- _printf.c
+++ _printf.c
@@ -6,62 +6,64 @@
  * str_len - to get the length of a string
  * @s: string
  * Return: returns the length
-*/
+ */
 
-int str_len (const char *s)
+int str_len(const char *s)
 {
-    int len = 0;
-    while (*s != '\0')
-    {
-        len++;
-        s++;
-    }
-    return (len);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
 
 /**
  * _printf - printf function
- * @format - format specified
- * Return - formatted output
-*/
+ * @format: format specified
+ * Return: formatted output
+ */
+
 int _printf(const char *format, ...)
 {
-    int charlength = 0;
-            checkForm formatchk[] = {
-                {'c', print_char},
-                {'s', print_string}
-            };
-            int i;
+	int charlength = 0;
+	checkForm formatchk[] = {
+		{'c', print_char},
+		{'s', print_string}
+	};
+	int i;
 
-    va_list arguments;
-    va_start(arguments, format);
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
 
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
+			for (i = 0; i < 2; i++)
+			{
+				if (*format == formatchk[i].opt)
+				{
+					formatchk[i].opt_func(arguments, &charlength);
+					break;
+				}
+			}
 
+		}
+		else
+		{
+			_putchar(*format);
+			charlength++;
+		}
+		format++;
+	}
 
-            for (i = 0; i < 2; i++)
-            {
-                if (*format == formatchk[i].opt)
-                {
-                    formatchk[i].opt_func(arguments, &charlength);
-                    break;
-                }
-            }
- 
-        }
-        else
-        {
-            _putchar(*format);
-            charlength++;
-        }
-        format++;
-    }
-
-    va_end(arguments);
-    return (charlength);
+	va_end(arguments);
+	return (charlength);
 }
