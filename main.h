98dbd9a09152607395ccc80c40d156d53ce30e4d
segunojo1 <olusegunkd2017@gmail.com>
2023-09-18 15:07:27 -0700

    betty iss

--- main.h
+++ main.h
@@ -4,15 +4,21 @@
 #include "stdlib.h"
 #include "stdarg.h"
 
-typedef struct
+/**
+ * struct chk - This struct will be used to filter which print
+ * function for i,c,s,d,x,X,o,p,u
+ * @opt: % format specifier to use
+ * @opt_func: function pointer to select the correct printer
+ */
+
+typedef struct chk
 {
-    char opt;
-    void (*opt_func)(va_list args, int *printed_chars);
-}checkForm;
- 
+	char opt;
+	void (*opt_func)(va_list args, int *printed_chars);
+} checkForm;
 
 int _putchar(char c);
-int str_len (const char *s);
+int str_len(const char *s);
 void print_char(va_list arguments, int *printed_chars);
 void print_string(va_list arguments, int *printed_chars);
 int _printf(const char *format, ...);
