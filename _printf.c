5b200e850066480d090bde65043d78754de3091c
segunojo1 <olusegunkd2017@gmail.com>
2023-09-19 00:32:25 +0100

    revert

--- _printf.c
+++ _printf.c
@@ -55,10 +55,6 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	if (format == NULL)
-	{
-		return (-1);
-	}
 	
 	while (*format != '\0')
 	{
