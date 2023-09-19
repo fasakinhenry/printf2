c6092939f752ca4d44e2e81c2d2c628b2db9676d
segunojo1 <olusegunkd2017@gmail.com>
2023-09-19 08:51:32 -0700

    betty done

--- print_char.c
+++ print_char.c
@@ -4,13 +4,13 @@
 /**
  * print_char - displays a character
  * @arguments: va_list arguments from _printf
- * @printed_chars: counts no of chars
  * Return: number of char printed
  */
 int print_char(va_list arguments)
 {
-	   int count;
-	   char get_char = va_arg(arguments, int);
-	  count = _putchar(get_char);
-	  return count; 
+	int count;
+	char get_char = va_arg(arguments, int);
+
+	count = _putchar(get_char);
+	return (count);
 }
