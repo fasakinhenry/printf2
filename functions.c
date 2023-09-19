4df43a22934442d8cae8ea85882bd30819ad565b
segunojo1 <olusegunkd2017@gmail.com>
2023-09-19 15:29:46 -0700

    betty

--- functions.c
+++ functions.c
@@ -159,7 +159,7 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 	n = va_arg(types, unsigned int);
-	m = 2147483648U; 
+	m = 2147483648U;
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
 	{
