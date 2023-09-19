9437f6139e9698704f87f46e070081b07957216a
segunojo1 <olusegunkd2017@gmail.com>
2023-09-19 08:58:52 -0700

    str

--- print_string.c
+++ print_string.c
@@ -4,24 +4,24 @@
 
 /**
  * print_string - a function to print out the characters
- * @args: variable value argument
- * @printed_chars: pointer to the formart specifier
+ * @arguments: variable value argument
  *
  * Return: numbers of printed characters || -1
  */
 int print_string(va_list arguments)
 {
-	        int count = 0;
-	        char *ptr = va_arg(arguments, char *);
-	        if (ptr == NULL)
-	        {
-	                ptr = "(null)";
-	        }
-	        while (*ptr)
-	        {
-	                count += _putchar(*ptr);
-	                ptr++;
-	        }
-	
-	        return count;
+	int count = 0;
+	char *ptr = va_arg(arguments, char *);
+
+	if (ptr == NULL)
+	{
+		ptr = "(null)";
+	}
+	while (*ptr)
+	{
+		count += _putchar(*ptr);
+		ptr++;
+	}
+
+	return (count);
 }
