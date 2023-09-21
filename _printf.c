40da8e869bb68c984c513dc24539aab4234a161a
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-21 01:33:38 +0100

    Change the variable i to il in the _printf.c file

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
+	int il, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -17,25 +17,24 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (il = 0; format && format[il] != '\0'; il++)
 	{
-		if (format[i] != '%')
+		if (format[il] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[il];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_prcn(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handles_printing(format, &i, list, buffer,
+			flags = get_flags(format, &il);
+			width = get_width(format, &il, list);
+			precision = get_prcn(format, &il, list);
+			size = get_size(format, &il);
+			++il;
+			printed = handles_printing(format, &il, list, buffer,
 					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
