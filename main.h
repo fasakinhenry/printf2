8cfc9cbdb6a2daf2717b2c960f60e2d10ea9551c
Fasakin Henry <fasakinhenry@gmail.com>
2023-09-21 01:22:17 +0100

    Edited some files

--- main.h
+++ main.h
@@ -31,20 +31,26 @@ int (*fn)(va_list, char[], int, int, int, int);
 * @fmt: The format.
 * @fm_t: The function associated.
 */
+
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handles_printing(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int
 size);
-/****************** FUNCTIONS ******************/
-/* Funtions to print chars and strings */
+
+/*** FUNCTIONS ***/
+
+/* Some funtions to print chars and strings */
+
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 int flags, int width, int precision, int size);
+
 /* Functions to print numbers */
+
 int print_int(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
